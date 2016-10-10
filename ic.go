9eb817242416d73e29d232be71e35643fce763e0
kimhanwoong <khw7096@gmail.com>
2016-10-10 21:35:27 +0900

    add go

---
+++ ic.go
@@ -0,0 +1,8 @@
+package ic
+
+import (
+	"image/jpeg"
+	"image/png"
+)
+
+func Resize //일단 필요한 기능이다.
---
+++ icmd.go
@@ -0,0 +1,10 @@
+package ic
+
+import (
+	"github.com/coldmine/ic"
+)
+
+func main() {
+	// 1. argv 선언.
+	// 2. 이미지를 컨버팅하도록 명령어를 처리한다.
+}
