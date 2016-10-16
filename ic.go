fb4e768d3613a0597558883baf489c2045c49cbb
kim yongbin <kybinz@gmail.com>
2016-10-17 01:17:01 +0900

    빈 width, height값 자동으로 계산

--- ic.go
+++ ic.go
@@ -5,12 +5,11 @@ import (
 	"image/jpeg"
 	_ "image/png"
 	"os"
+
 	"golang.org/x/image/draw"
 )
 
 func Resize(srcpath, dstpath string, width, height int) error {
-	// TODO: 만약 width값이 0 이라면 height값을 기준으로 autoWidth값을 가지고 온다.
-	// TODO: 만약 height값이 0 이라면 width값을 기준으로 autoHeight값을 가지고 온다.
 	reader, err := os.Open(srcpath)
 	if err != nil {
 		return err
@@ -19,6 +18,16 @@ func Resize(srcpath, dstpath string, width, height int) error {
 	src, _, err := image.Decode(reader)
 	if err != nil {
 		return err
+	}
+
+	// 빈 width, height 값 자동으로 계산
+	if width == 0 || height == 0 {
+		s := src.Bounds().Max
+		if width == 0 {
+			width = int(float32(height) / float32(s.Y) * float32(s.X))
+		} else {
+			height = int(float32(width) / float32(s.X) * float32(s.Y))
+		}
 	}
 
 	dst := image.NewRGBA(image.Rect(0, 0, width, height))
